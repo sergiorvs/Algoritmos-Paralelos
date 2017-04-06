876bf8cf2f13690cc12c8768cf3f77695edd171a
sergiorvs <segio.rolando@gmail.com>
2017-04-06 12:15:51 -0500

    respuesta

--- MPI/greeting.c
+++ MPI/greeting.c
@@ -16,7 +16,7 @@ int main(void)
 
     if (my_rank != 0) {
     	sprintf(greeting, "Greetings from process %d of %d!", my_rank, comm_sz);
-    	MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    	MPI_Send(greeting, MAX_STRING, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }else{
     	 printf("Greetings from process %d of %d!\n", my_rank,comm_sz);
 		 for (int q = 1; q < comm_sz; q++) {
@@ -29,6 +29,17 @@ int main(void)
 MPI_Finalize();
 return 0;
 
-
-
 }
+
+/*
+What happens in the greetings program if, instead of strlen(greeting) + 1,
+we use strlen(greeting) for the length of the message being sent by pro-
+cesses 1, 2, . . . , comm sz−1? What happens if we use MAX STRING instead of
+strlen(greeting) + 1? Can you explain these results?
+
+Respuesta: 
+-> El programa no sabria cuando es el final de la linea asi que sería bloqueado
+-> Si se usara Max_String no pasaria nada porque el mensaje siempre estara dentro
+de ese rango.
+
+*/
