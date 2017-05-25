6ee47459fb4a294d30e495dbd1efdef051509e2d
sergiorvs <segio.rolando@gmail.com>
2017-05-25 00:46:57 -0500

    cuda

---
+++ Cuda/matAddFil.cu
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+__global__
+void matAddKernel(float *A, float *B, float *C, int n){
+  int i = threadIdx.x + blockDim.x * blockIdx.x, j;
+  if(i < n){
+    for(j = 0; j < n; j++){
+       C[i*n+j] = A[i*n+j] + B[i*n+j];
+    }
+  }
+  
+}
+
+
+void matAdd(float* A, float* B, float* C, int n){
+  int size = n*n*sizeof(float);
+  float *d_A, *d_B, *d_C;
+
+  cudaMalloc((void **) &d_A, size);
+  cudaMemcpy(d_A,A,size,cudaMemcpyHostToDevice);
+  cudaMalloc((void **) &d_B, size);
+  cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
+  cudaMalloc((void **) &d_C, size);
+
+  matAddKernel<<<ceil(n/256.0), 256>>>(d_A,d_B,d_C,n);
+  
+  cudaMemcpy(C,d_C,size,cudaMemcpyDeviceToHost);
+
+  cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
+
+}
+
+int main(){
+  int n,i,j;
+  float *h_A,*h_B,*h_C;
+  printf("ingrese el tamaño de la matriz:\n");
+  scanf("%d", &n);
+  h_A = (float*) malloc(n*n*sizeof(float));
+  h_B = (float*) malloc(n*n*sizeof(float));
+  h_C = (float*) malloc(n*n*sizeof(float));
+  for(i = 0; i < n; i++){
+    for(j = 0; j < n; j++){
+      h_A[i*n+j] = 1;
+      h_B[i*n+j] = 1;
+    }
+  }
+  matAdd(h_A,h_B,h_C,n);
+  for(i = 0; i < n; i++){
+    for(j = 0; j < n; j++){
+        printf("%f ", h_C[i*n+j]);
+    }
+    printf("\n"); 
+  }
+  printf("\n");
+  return 0;
+}
